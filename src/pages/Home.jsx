e7b6a8eb6f3aec74660cc042e4e2c5b0b46daa87
Pravin Patrike <itspravinpatrike@gmail.com>
2023-12-26 22:42:38 +0530

    Navbar complete
    Started building Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+    POPUP
+</div> */}
 
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
-        <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-            POPUP
-        </div>
-      
+      <Canvas className='w-full h-screen bg-transparent'>
+        
+      </Canvas>
     </section>
   )
 }
