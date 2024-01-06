c00c69f0fac1f2e20e16d5952034dc8b7e09a347
Pravin Patrike <itspravinpatrike@gmail.com>
2024-01-06 14:30:28 +0530

    working on about

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,60 +1,3 @@
-// import React from 'react'
-// import {arrow} from '../assets/icons'
-// import { Link } from 'react-router-dom'
-
-// const InfoBox = ({ text, link, btnText }) => {
-//     <div>
-//         <p className='font-medium sm:text-xl text-center'>{text}</p>
-//         <Link to={link} className="neo-brutalism-white neo-btn">
-//             {btnText}
-//             <img src={arrow} className='w-4 h-4 object-contain'/>
-//         </Link>
-//     </div>
-// }
-
-
-// const renderContent = {
-//     1: (
-//         <h1 className='sm:text-xl sm:leading-snug text-center 
-//         neo-brutalism-blue py-4 px-8 text-white mx-5'
-//         >
-//             Hi, I am <span className='font-semibold'> Pravin</span>👋
-//             <br/>
-//             A software Engineer from India
-//         </h1>
-//     ),
-//     2: (
-//         <InfoBox 
-//         text="worked with many companies and picked up many skills aling the way"
-//         link="/about"
-//         btnText="Learn more"
-//         />
-//     ),
-//     3: (
-//         <InfoBox 
-//         text="Built various projects during the period. Curious about the impact?"
-//         link="/projects"
-//         btnText="visit my portfolio!"
-//         />
-//     ),
-//     4: (
-//         <InfoBox 
-//         text="Need a project done or looking for a dev? I'm just a few keystrokes away!"
-//         link="/contact"
-//         btnText="Let's Talk!"
-//         />
-//     )
-// }
-
-
-
-
-// const HomeInfo = ({ currentStage }) => {
-//     return renderContent[currentStage] || null
-// }
-
-// export default HomeInfo
-
 
 import { Link } from "react-router-dom";
 
@@ -68,7 +11,7 @@ const HomeInfo = ({ currentStage }) => {
                 >
                     Hi, I am <span className='font-semibold'> Pravin</span>👋
                     <br/>
-                    A software Engineer from India
+                    A software Engineer from <span className='bg-gradient-to-r from-orange-600 via-blue-800 to-green-700 inline-block text-transparent bg-clip-text font-bold '>India</span>
                 </h1>
             )
 
